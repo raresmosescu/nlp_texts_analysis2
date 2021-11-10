c49ef4509102d04d4b9f91f15447a184c42e24da
Claudiu <clau_123@yahoo.com>
2021-11-10 18:30:59 +0200

    changes

--- app/core/views.py
+++ app/core/views.py
@@ -11,10 +11,26 @@ def home(request):
     return render(request, 'index.html')
 
 def results(request):
-    text = request.POST.get('text')
-    print(text, bool(text))
-    if text:
-        r = analyze_text(text).most_common(60)
+    if request.method == 'POST':
+      input_method = request.POST.get('input-method')
+      print(input_method)
+      # print(text, bool(text))
+      print(request.POST)
+      if input_method == '1':
+        text = request.POST.get('text')
+        if text:
+          r = analyze_text(text).most_common(60)
+        else:
+          r = analyze_text(samples.sample1).most_common(60)
+        return render(request, 'results.html', context={'results': r, 'results_str': str(dict(r))})
+        
+      else:
+        file = str(request.FILES['file'].read())
+        if file:
+          r = analyze_text(file).most_common(60)
+        else:
+          r = analyze_text(samples.sample1).most_common(60)
+        return render(request, 'results.html', context={'results': r, 'results_str': str(dict(r))})
+          
     else:
-        r = analyze_text(samples.sample1).most_common(60)
-    return render(request, 'results.html', context={'results': r, 'results_str': str(dict(r))})
+      return HttpResponse('i donno man')
