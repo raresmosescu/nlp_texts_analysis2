7f85369dcd3ea06b7eddb80b5ed9a9603a3168b3
Rares Mosescu <raresmose@gmail.com>
2021-11-12 15:53:03 +0200

    face lift

--- app/app/settings.py
+++ app/app/settings.py
@@ -126,3 +126,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'core/static'
+]
